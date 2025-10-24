f505cd10511766bbd7b49ca64dd61af92b07bd1a
gustn99 <azerosong@gmail.com>
2025-10-24 14:29:03 +0900

    fix: validate input을 실행하는 validated input 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { input } from "./utils/input/input.js";
+import { validatedInput } from "./utils/input/input.js";
 import { getValidNames } from "./utils/input/name.js";
 import { getValidCount } from "./utils/input/count.js";
 
 class App {
   async run() {
-    const nameString = await input(
+    const nameString = await validatedInput(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
     );
     const validNames = getValidNames(nameString);
 
-    const countString = await input("시도할 횟수는 몇 회인가요?");
+    const countString = await validatedInput("시도할 횟수는 몇 회인가요?");
     const validCount = getValidCount(countString);
   }
 }
--- src/utils/input/input.js
+++ src/utils/input/input.js
@@ -1,5 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 
+export const validatedInput = async (question) => {
+  const userInput = await input(question);
+  validateInput(userInput);
+  return userInput;
+};
+
 export const input = async (question) => {
   const userInput = await Console.readLineAsync(`${question}\n`);
   return userInput;
